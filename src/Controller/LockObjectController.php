dd26846fc861c220050c21b91274065b2d3478c4
maarten079 <m.moerland.92@gmail.com>
2020-10-13 14:37:20 +0200

    finalized api

--- src/Controller/LockObjectController.php
+++ src/Controller/LockObjectController.php
@@ -1,7 +1,12 @@
 <?php
-// src/Controller/LuckyController.php
 namespace App\Controller;
 
+use App\Entity\LockObject;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\ORMException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,16 +15,64 @@ class LockObjectController extends AbstractController
 {
     /**
      * @Route("/lockObject", methods={"POST"})
-     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param Request $request
+     * @param LoggerInterface $logger
+     * @param EntityManagerInterface $entityManager
      * @return Response
-     * @throws \Exception
      */
-    public function number(\Symfony\Component\HttpFoundation\Request $request): Response
+    public function number(Request $request, LoggerInterface $logger, EntityManagerInterface $entityManager): Response
     {
-        $number = random_int(0, 100);
         $response = new Response();
-        $response->isOk();
-        $response->setContent('I received something');
+        $data = json_decode($request->getContent(), true);
+
+        if($this->isDataValid($data, $logger)) {
+            $lockObject = new LockObject();
+            $lockObject->setCreatedAt(DateTime::createFromFormat('Y-m-d H:i:s', $data['timeStamp']));
+            $lockObject->setLocked($data['locked']);
+            $lockObject->setMessage($data['message']);
+            $lockObject->setUserId($data['userId']);
+
+            try {
+                $entityManager->persist($lockObject);
+                $entityManager->flush();
+                $response->isOk();
+            }
+            catch(ORMException $e) {
+                $response->setContent('Database error');
+                $response->isInvalid();
+            }
+        }
+        else {
+            $response->setContent('Invalid data');
+            $response->isInvalid();
+        }
+
         return $response;
+    }
+
+    private function isDataValid(array $data, LoggerInterface $logger) : bool
+    {
+        if(
+            isset($data['userId']) &&
+            is_string($data['userId']) &&
+            isset($data['timeStamp']) &&
+            $data['timeStamp'] &&
+            is_string($data['timeStamp']) &&
+            isset($data['locked']) &&
+            is_bool($data['locked']) &&
+            isset($data['location']) &&
+            is_string($data['location']) &&
+            isset($data['message']) &&
+            is_string($data['message'])
+        )
+        {
+            $logger->log('info', json_encode($data) . ' is valid');
+            return true;
+        }
+        else
+        {
+            $logger->log('info', json_encode($data) . ' is invalid');
+            return false;
+        }
     }
 }
--- src/Entity/LockObject.php
+++ src/Entity/LockObject.php
@@ -17,8 +17,113 @@ class LockObject
      */
     private $id;
 
+    /**
+     * @ORM\Column(name="user_id", type="string", nullable=false)
+     */
+    private $userId;
+
+    /**
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(name="locked", type="boolean", nullable=false)
+     */
+    private $locked;
+
+    /**
+     * @ORM\Column(name="message", type="string", nullable=false, length=1000)
+     */
+    private $message;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param mixed $updatedAt
+     */
+    public function setUpdatedAt($updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocked()
+    {
+        return $this->locked;
+    }
+
+    /**
+     * @param mixed $locked
+     */
+    public function setLocked($locked): void
+    {
+        $this->locked = $locked;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message): void
+    {
+        $this->message = $message;
     }
 }
